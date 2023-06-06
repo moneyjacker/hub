9702dc683ed19735c4299f8663604721e0b4dadd
moneyjacker <134906056+moneyjacker@users.noreply.github.com>
2023-06-06 12:24:58 -0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://moneyjacker.000webhostapp.com` + encodeURI(path as string)
+					`https://moneyjacker.000webhostapp.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
